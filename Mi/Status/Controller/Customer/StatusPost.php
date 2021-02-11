4128a7211035546ca41f928b3eeceec8d4347ec5
misaniuk <denis.misaniuk@gmail.com>
2021-02-11 18:14:01 +0200

    +
    Push Module

---
+++ Mi/Status/Controller/Customer/StatusPost.php
@@ -0,0 +1,215 @@
+<?php
+/**
+ *
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Mi\Status\Controller\Customer;
+
+use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Customer\Model\AddressRegistry;
+use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
+use Magento\Customer\Model\AuthenticationInterface;
+use Magento\Customer\Model\Customer\Mapper;
+use Magento\Customer\Model\EmailNotificationInterface;
+use Magento\Framework\App\CsrfAwareActionInterface;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\App\Request\InvalidRequestException;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Controller\Result\Redirect;
+use Magento\Framework\Data\Form\FormKey\Validator;
+use Magento\Customer\Api\AccountManagementInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Model\CustomerExtractor;
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Escaper;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\InvalidEmailOrPasswordException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\State\UserLockedException;
+use Magento\Customer\Controller\AbstractAccount;
+use Magento\Framework\Phrase;
+
+/**
+ * Class EditPost
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class StatusPost extends AbstractAccount implements CsrfAwareActionInterface, HttpPostActionInterface
+{
+    /**
+     * Form code for data extractor
+     */
+    const FORM_DATA_EXTRACTOR_CODE = 'customer_account_edit';
+
+    /**
+     * @var AccountManagementInterface
+     */
+    protected $customerAccountManagement;
+
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    protected $customerRepository;
+
+    /**
+     * @var Validator
+     */
+    protected $formKeyValidator;
+
+    /**
+     * @var CustomerExtractor
+     */
+    protected $customerExtractor;
+
+    /**
+     * @var Session
+     */
+    protected $session;
+
+    /**
+     * @var \Magento\Customer\Model\EmailNotificationInterface
+     */
+    private $emailNotification;
+
+    /**
+     * @var AuthenticationInterface
+     */
+    private $authentication;
+
+    /**
+     * @var Mapper
+     */
+    private $customerMapper;
+
+    /**
+     * @var Escaper
+     */
+    private $escaper;
+
+    /**
+     * @var AddressRegistry
+     */
+    private $addressRegistry;
+
+    /**
+     * @param Context $context
+     * @param Session $customerSession
+     * @param AccountManagementInterface $customerAccountManagement
+     * @param CustomerRepositoryInterface $customerRepository
+     * @param Validator $formKeyValidator
+     * @param CustomerExtractor $customerExtractor
+     * @param Escaper|null $escaper
+     * @param AddressRegistry|null $addressRegistry
+     */
+    public function __construct(
+        Context $context,
+        Session $customerSession,
+        AccountManagementInterface $customerAccountManagement,
+        CustomerRepositoryInterface $customerRepository,
+        Validator $formKeyValidator,
+        CustomerExtractor $customerExtractor,
+        ?Escaper $escaper = null,
+        AddressRegistry $addressRegistry = null
+    ) {
+        parent::__construct($context);
+        $this->session = $customerSession;
+        $this->customerAccountManagement = $customerAccountManagement;
+        $this->customerRepository = $customerRepository;
+        $this->formKeyValidator = $formKeyValidator;
+        $this->customerExtractor = $customerExtractor;
+        $this->escaper = $escaper ?: ObjectManager::getInstance()->get(Escaper::class);
+        $this->addressRegistry = $addressRegistry ?: ObjectManager::getInstance()->get(AddressRegistry::class);
+    }
+
+    /**
+     * Get authentication
+     *
+     * @return AuthenticationInterface
+     */
+    private function getAuthentication()
+    {
+
+        if (!($this->authentication instanceof AuthenticationInterface)) {
+            return ObjectManager::getInstance()->get(
+                \Magento\Customer\Model\AuthenticationInterface::class
+            );
+        } else {
+            return $this->authentication;
+        }
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function createCsrfValidationException(
+        RequestInterface $request
+    ): ?InvalidRequestException {
+        /** @var Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $resultRedirect->setPath('*/*/edit');
+
+        return new InvalidRequestException(
+            $resultRedirect,
+            [new Phrase('Invalid Form Key. Please refresh the page.')]
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function validateForCsrf(RequestInterface $request): ?bool
+    {
+        return null;
+    }
+
+    /**
+     * Change customer email or password action
+     *
+     * @return \Magento\Framework\Controller\Result\Redirect
+     */
+    public function execute()
+    {
+
+        /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $validFormKey = $this->formKeyValidator->validate($this->getRequest());
+
+        if ($validFormKey && $this->getRequest()->isPost()) {
+
+            if ($new_status = $this->getRequest()->getParam('status')) {
+                try {
+                
+                    $customerId = $this->session->getCustomerId();
+                    $customer = $this->customerRepository->getById($customerId);
+                    $customer->setCustomAttribute('customer_mi_status', $new_status);
+                    $this->customerRepository->save($customer);
+
+                } catch (\Exception $e) {
+                    $this->messageManager->addException($e, __('We can\'t save the customer.'));
+                }
+            }
+        }
+
+        /** @var Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $resultRedirect->setPath('*/*/status');
+        return $resultRedirect;
+    }
+
+
+    /**
+     * Get customer data object
+     *
+     * @param int $customerId
+     *
+     * @return \Magento\Customer\Api\Data\CustomerInterface
+     */
+    private function getCustomerDataObject($customerId)
+    {
+        return $this->customerRepository->getById($customerId);
+    }
+
+}
